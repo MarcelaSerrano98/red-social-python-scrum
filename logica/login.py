166293a6fa2e7cce21089c8982655fb66cd60c5b
Joan Jaimes <Sebasq39@gmail.com>
2025-07-08 11:06:46 -0500

    refactor: :recycle: refactorizacion de codigo

--- logica/login.py
+++ logica/login.py
@@ -1,4 +1,6 @@
 from jsons.json_utils import leer_json
+from menu.menus import menu_principal
+from logica.registro import menu_registro_log
 
 DATA_FILE = "savefiles/users.json"
 
@@ -16,3 +18,33 @@ def iniciar_sesion():
 
     print("❌ Usuario o contraseña incorrectos.")
     return None
+
+def menu_inicial_log():
+    from menu.menus import menu_inicial, menu_registro, menu_login
+    while True:
+        menu_inicial()
+        opcion = input("Selecciona una opción: ")
+        if opcion == "1":
+            menu_registro_log()
+        elif opcion == "2":
+            iniciar_sesion()
+        elif opcion == "3":
+            print("👋 Gracias por usar la red social. ¡Hasta pronto!")
+            break
+        else:
+            print("❌ Opción inválida. Intenta de nuevo.") 
+
+
+def menu_login_log():
+    while True:
+        
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            usuario = iniciar_sesion()
+            menu_principal(usuario)      
+        elif opcion == "2":
+            print("👋 Regresando...")
+            break
+        else:
+            print("❌ Opción inválida.")
--- menu/menus.py
+++ menu/menus.py
@@ -1,70 +1,30 @@
 from logica.crearPublicaciones import crear_publicacion
 from logica.registro import agregrar_datos
 from jsons.json_utils import leer_json
-from logica.login import iniciar_sesion
 from logica.listado_de_usuarios import lista
 from logica.verPublicaciones import ver_publicaciones
 from logica.misPublicaciones import ver_perfil
 
 def menu_inicial():
-    while True:
         print("1. Registrarse")
         print("2. Iniciar sesión")
         print("3. Salir")
-
-        opcion = input("Selecciona una opción: ")
-
-        if opcion == "1":
-            menu_registro()
-        elif opcion == "2":
-            menu_login()
-        elif opcion == "3":
-            print("👋 Gracias por usar la red social. ¡Hasta pronto!")
-            break
-        else:
-            print("❌ Opción inválida. Intenta de nuevo.") 
 
 
 DATA_FILE = "savefiles/users.json"
 
 def menu_registro():
-    while True:
         print("\n===== MENÚ DE REGISTRO DE USUARIO =====")
         print("1. Registrar nuevo usuario")
         print("2. Ver todos los usuarios")
         print("3. Salir")
 
-        opcion = input("Selecciona una opción: ")
-
-        if opcion == "1":
-            agregrar_datos()
-        elif opcion == "2":
-            usuarios = leer_json(DATA_FILE)
-            print("\nUsuarios registrados:")
-            for usuario in usuarios:
-                print(f"ID: {usuario['ID']}, Nombre: {usuario['Nombre']}, Edad: {usuario['Anhos']}")
-        elif opcion == "3":
-            print("👋 Saliendo del menú de registro...")
-            break
-        else:
-            print("❌ Opción inválida. Intenta de nuevo.")
-
 def menu_login():
-    while True:
-        print("\n===== MENÚ DE LOGIN =====")
-        print("1. Iniciar sesión")
-        print("2. Volver al menú anterior")
+         
+         print("\n===== MENÚ DE LOGIN =====")
+         print("1. Iniciar sesión")
+         print("2. Volver al menú anterior")
 
-        opcion = input("Selecciona una opción: ")
-
-        if opcion == "1":
-            usuario = iniciar_sesion()
-            menu_principal(usuario)      
-        elif opcion == "2":
-            print("👋 Regresando...")
-            break
-        else:
-            print("❌ Opción inválida.")
 
 def menu_principal(usuario):
     while True:
@@ -102,7 +62,6 @@ def menu_principal(usuario):
             break
             
             
-
 
 def menu_interactuar():
     print("\n===== INTERACTUAR CON PUBLICACIONES =====")
